5320be1276aaf9d8c1ed7d40d3cec5ae72d54cac
Markus L <34713459+theWoodspirit@users.noreply.github.com>
2021-10-12 11:15:08 +0200

    initial push
    asasas

---
+++ cmake-build-debug/CMakeFiles/info3.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/info3.dir/main.cpp.obj"
+  "info3.exe"
+  "info3.exe.manifest"
+  "info3.pdb"
+  "libinfo3.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/info3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
